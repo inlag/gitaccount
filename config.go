5d9e1b1bb6fbbddf55c9165d2fe9c3ed7df7ed22
Sergey Khorbin <khorbin96@gmail.com>
2024-01-04 15:20:27 +0200

    feat: added reading of configuration file

--- config.go
+++ config.go
@@ -1,19 +1,70 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
+)
+
 type Config struct {
-	Users []User `yaml:"Users"`
+	f     *os.File
+	users []User `yaml:"users"`
 }
 
 func NewConfig() (*Config, error) {
-	// todo: add handling
-	return &Config{}, nil
+	file, err := GetConfigFile()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed is open configuration file")
+	}
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		return nil, errors.Wrap(err, "reading configuration file is failed")
+	}
+
+	var users []User
+	err = yaml.Unmarshal(fileBytes, &users)
+	if err != nil {
+		return nil, errors.Wrap(err, "configuration file structure is invalid")
+	}
+
+	return &Config{
+		f:     file,
+		users: users,
+	}, nil
 }
 
 func (c *Config) GetUsers() []User {
-	return c.Users
+	return c.users
 }
 
 func (c *Config) SaveUserInfo(name string, email string) error {
 	//TODO implement me
 	panic("implement me")
+}
+
+func GetConfigFile() (*os.File, error) {
+	pathToFile := GetConfigPath()
+	file, err := os.OpenFile(pathToFile, os.O_RDWR, 0777)
+	if errors.Is(err, os.ErrNotExist) {
+		file, err = os.Create(pathToFile)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func GetConfigPath() string {
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return fmt.Sprintf("%s%c%s", dirname, os.PathSeparator, ".gitacc")
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.0
 require (
 	github.com/pkg/errors v0.9.1
 	github.com/urfave/cli/v2 v2.25.7
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
